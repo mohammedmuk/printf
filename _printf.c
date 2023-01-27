5226e6886a0972cc02669e44eb7458335b28c9b0
Mohammedmuk <mohammedmuk20@gmail.com>
2023-01-27 11:44:53 -0800

    new modify

--- _printf.c
+++ _printf.c
@@ -11,43 +11,60 @@
 
 int _printf(const char *format, ...)
 {
-	int i, j, num = 0;
-	char *s;
+	va_list valist;
+	int i, j, length = 0;
+	char *str;
 
-	va_list args;
-
+	if (!format || (format[0] == '%' && format[1] == '\0') ||
+	    format == NULL || format == 0)
+		return (-1);
 	va_start(args, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && (format[i + 1] == 's'
-		|| format[i + 1] == 'c' || format[i + 1] == 'd' || format[i + 1] == 'i'))
+		|| format[i + 1] == 'c' || format[i + 1] == 'd'
+		|| format[i + 1] == 'i'))
 		{
 			if (format[i + 1] == 's')
 			{
-				s = va_arg(args, char *);
-
-				for (j = 0; s[j]; j++)
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				for (j = 0; str[j]; j++)
 				{
-					fprintf(stdout, "%c", s[j]);
-					num++;
+					fprintf(stdout, "%c", str[j]);
+					length++;
 				}
+				length -= 1;
 			}
-
 			if (format[i + 1] == 'c')
-			{
 				fprintf(stdout, "%c", va_arg(args, int));
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				int num = va_arg(args, int);
+				fprintf(stdout, "%d", num);
+				if (num == 0)
+					return (1);
+				if (num < 0)
+				{
+					num *= (-1);
+					num ++;
+				}
+				while (num > 0 || num < 0)
+				{
+					num = num / 10;
+					length++;
+				}
+				length -= 1;
 			}
+			if (format[i + 1] == '%')
+				fprintf(stdout, "%%"):
+			i += 1;
 		}
-
 		else
-		{
 			fprintf(stdout, "%c", format[i]);
-			num++;
-		}
+		length++;
 	}
-
 	va_end(args);
-
-	return (num);
+	return (length);
 }
